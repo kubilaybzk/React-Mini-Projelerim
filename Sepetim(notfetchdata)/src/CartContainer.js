8dd5169eec727c7a1a0409eb207d6b25e94c6945
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2021-11-29 16:24:16 +0300

    Sepetim(Fetch Data)

--- Sepetim(notfetchdata)/src/CartContainer.js
+++ Sepetim(notfetchdata)/src/CartContainer.js
@@ -3,11 +3,7 @@ import CartItem from './CartItem'
 import { useGlobalContext } from './context'
 
 const CartContainer = () => {
-  const { cart ,total,clearCart} = useGlobalContext()
-
-
-
-
+  const { cart, total, clearCart } = useGlobalContext()
   if (cart.length === 0) {
     return (
       <section className='cart'>
@@ -39,10 +35,7 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button
-          className='btn clear-btn'
-          onClick={clearCart}
-        >
+        <button className='btn clear-btn' onClick={clearCart}>
           clear cart
         </button>
       </footer>
--- Sepetim(notfetchdata)/src/context.js
+++ Sepetim(notfetchdata)/src/context.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
-import CartItem from './CartItem'
 import cartItems from './data'
 import reducer from './reducer'
 // ATTENTION!!!!!!!!!!
@@ -7,45 +6,44 @@ import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
-
-const initialState={
-  loading:false,
-  cart:cartItems,
-  total:0,
-  amount:0,
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
 }
 
-
-
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer,initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  const clearCart=()=>{
-    dispatch({type:'CLEAR_CART'})
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' })
   }
-
-  const remove =(id)=>{
-    dispatch({type:'REMOVE',payload:id})
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id })
   }
-
-
-  const increase =(id)=>{
-    dispatch({type:"INCREASE",payload:id})
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id })
   }
-
-  const decrease=(id)=>{
-    dispatch({type:"DECREASE",payload:id})
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id })
   }
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  }
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } })
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   useEffect(() => {
     dispatch({ type: 'GET_TOTALS' })
   }, [state.cart])
-
-  const toggleAmount = (id, type) => {
-    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } })
-  }
-
-
   return (
     <AppContext.Provider
       value={{
@@ -54,7 +52,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
-        toggleAmount
+        toggleAmount,
       }}
     >
       {children}
